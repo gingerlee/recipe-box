4298ea9defe393fac47e68970d033e762300a9da
Cat Janowitz and Ginger Lee Kretschmer <ginger@>
2018-01-16 11:58:44 -0800

    try to adapt recipe box to task list from homework

---
+++ app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html'
+})
+
+export class AppComponent {
+  currentRecipe: string = 'New Recipe';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  recipes: Recipe[] = [
+    new Recipe('Vegan Lasagna with Cashew Ricotta Cheese', '2 hours', ['vegan lasagna noodles', 'tomatoes', 'onions'], ['do this', 'then this', 'finally this']),
+    new Recipe('Meat Lasagna with Cashew Ricotta Cheese', '2 hours', ['vegan lasagna noodles', 'tomatoes', 'onions'], ['do this', 'then this', 'finally this']),
+    new Recipe('Veggie Lasagna with Cashew Ricotta Cheese', '2 hours', ['vegan lasagna noodles', 'tomatoes', 'onions'], ['do this', 'then this', 'finally this']),
+  ];
+}
+
+export class Recipe {
+  constructor(public title: string, public cooktime: string, public ingredients: string[], public directions: string[], ) { }
+}
